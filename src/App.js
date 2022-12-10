e5ab833281485e89f13311d0ff621150077a087b
Prathamesh dhasade <prathameshdhasade2015@gmail.com>
2022-12-10 16:25:22 +0530

    quiz website with an api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux";
+
+import Settings from "./Components/Settings";
+import Question from "./Components/Question";
+import FinalScreen from "./Components/FinalScreen";
+
+import "./App.css";
 
 function App() {
+  const questions = useSelector((state) => state.questions);
+  const questionIndex = useSelector((state) => state.index);
+
+  let component;
+
+  if (questions.length && questionIndex + 1 <= questions.length) {
+    component = <Question />;
+  } else if (!questions.length) {
+    component = <Settings />;
+  } else {
+    component = <FinalScreen />;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-container">{component}</div>
     </div>
   );
 }
